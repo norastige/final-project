eeedf348742772cb4c79cf46c9ee0deab5a971c3
Nora Stige <norastige@Nora-sin-MacBook-Pro.local>
2024-12-05 18:11:34 +0100

    create index page plus accordion

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,12 @@
 const toggleMenuButton = document.querySelector(".navbar__toggle-button");
 const navbarLinksContainer = document.querySelector(".navbar__links");
 const links = document.querySelectorAll(".navbar__link");
+const titleContainers = document.querySelectorAll(
+  ".accordion__title-container"
+);
+const descriptionContainers = document.querySelectorAll(
+  ".accordion__description-container"
+);
 
 //
 toggleMenuButton.addEventListener("click", () => {
@@ -14,5 +20,21 @@ links.forEach((link) => {
   link.addEventListener("click", (e) => {
     links.forEach((link) => link.classList.remove("navbar__link-active"));
     e.currentTarget.classList.add("navbar__link-active");
+  });
+});
+
+// Accordion functionality
+// Access the arrow
+const accordionToggleLogo = document.querySelectorAll(
+  ".accordion__toggle-logo"
+);
+
+// Event Listener to Each of the titles (bruker for each loop)
+titleContainers.forEach((title, index) => {
+  title.addEventListener("click", () => {
+    descriptionContainers[index].classList.toggle(
+      "accordion__description-container--active"
+    );
+    accordionToggleLogo[index].classList.toggle("rotate-arrow");
   });
 });
